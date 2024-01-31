86081d7bd011c79f8b8760834ae9777c7584aa6f
giovancombo <gionni.combo@gmail.com>
2024-01-31 02:07:23 +0100

    Lab3 code

--- lab3/policy.py
+++ lab3/policy.py
@@ -1,13 +1,14 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 # A simple, but generic, policy network with one hidden layer.
 class PolicyNet(nn.Module):
-    def __init__(self, env):
+    def __init__(self, env, hs):
         super().__init__()
-        self.fc1 = nn.Linear(env.observation_space.shape[0], 128)
-        self.fc2 = nn.Linear(128, 128)
-        self.fc3 = nn.Linear(128, env.action_space.n)
+        self.fc1 = nn.Linear(env.observation_space.shape[0], hs)
+        self.fc2 = nn.Linear(hs, hs)
+        self.fc3 = nn.Linear(hs, env.action_space.n)
         self.relu = nn.ReLU()
         
     def forward(self, s):
