9d05259903ccba8f1cda33dd778cc9ffce49ae35
Giovanni Colombo <giovanni.colombo@edu.unifi.it>
2024-07-28 11:31:17 +0200

    Update models.py

--- lab1/models.py
+++ lab1/models.py
@@ -1,6 +1,6 @@
 # Deep Learning Applications 2023 course, held by Professor Andrew David Bagdanov - University of Florence, Italy
 # Created by Giovanni Colombo - Mat. 7092745
-# Dedicated Repository on GitHub at https://github.com/giovancombo/DLA_Labs/tree/main/lab2
+# Dedicated Repository on GitHub at https://github.com/giovancombo/DLA_Labs/tree/main/lab1
 
 import torch
 import torch.nn as nn
