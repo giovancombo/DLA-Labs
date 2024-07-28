e6adaf79a82d8cc03f87e1e196be4e29039b7441
Giovanni Colombo <giovanni.colombo@edu.unifi.it>
2024-07-28 20:49:30 +0200

    Update my_ppo2.py

--- lab3/my_ppo2.py
+++ lab3/my_ppo2.py
@@ -11,6 +11,7 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.distributions.categorical import Categorical
 import matplotlib.pyplot as plt
+import wandb
 
 
 class PPOMemory:
@@ -206,6 +207,42 @@ class Agent():
                 self.critic.optimizer.step()
 
         self.memory.clear_memory()  
+
+
+def train_ppo2(env, agent, N, n_games, wandb_log):
+    figure_file = f'images/{env.unwrapped.spec.id}_myppo2.png'
+    best_score = env.reward_range[0]
+    score_history = []
+    learn_iters, avg_score, n_steps = 0, 0, 0
+
+    for i in range(n_games):
+        observation, info = env.reset()
+        done = False
+        score = 0
+        while not done:
+            action, prob, val = agent.choose_action(observation)
+            observation_, reward, terminated, truncated, info = env.step(action)
+            done = terminated or truncated
+            n_steps += 1
+            score += reward
+            agent.remember(observation, action, prob, val, reward, done)
+            if n_steps % N == 0:
+                agent.learn()
+                learn_iters += 1
+            observation = observation_
+        score_history.append(score)
+        avg_score = np.mean(score_history[-100:])
+
+        if wandb_log:
+            wandb.log({'score': score}, step = i)
+
+        if avg_score > best_score:
+            best_score = avg_score
+            agent.save_models()
+
+        print('episode', i, 'score %.1f' % score, 'avg score %.1f' % avg_score, 'time_steps', n_steps, 'learning_steps', learn_iters)
+    x = [i+1 for i in range(len(score_history))]
+    plot_learning_curve(x, score_history, figure_file)
 
 
 def plot_learning_curve(x, scores, figure_file):
