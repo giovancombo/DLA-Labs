470abb98a054bc9a809fb3d726fad0617f362117
Giovanni Colombo <giovanni.colombo@edu.unifi.it>
2024-07-28 18:42:58 +0200

    Update and rename combo_reinforce.py to my_reinforce.py

--- lab3/my_reinforce.py
+++ lab3/my_reinforce.py
@@ -1,4 +1,8 @@
-# Implementation of the REINFORCE algorithm, starting from the baseline code from professor Bagdanov
+# Deep Learning Applications 2023 course, held by Professor Andrew David Bagdanov - University of Florence, Italy
+# Created by Giovanni Colombo - Mat. 7092745
+# Dedicated Repository on GitHub at https://github.com/giovancombo/DLA_Labs/tree/main/lab3
+
+# My implementation of the REINFORCE algorithm, starting from the baseline code from professor Bagdanov
 
 import torch
 from torch.distributions import Categorical
@@ -36,12 +40,11 @@ def optimize_policy(log_probs, discountedRewards):
 
 # The actual REINFORCE algorithm
 def combo_reinforce(env, policy, lr = 1e-2, gamma = 0.999, episodes = 10, device = 'cpu', wandb_log = False):
-
     optimizer = torch.optim.Adam(policy.parameters(), lr = lr)
     running_rewards = [0.0]
 
     if wandb_log:
-        wandb.watch(policy, log="all", log_freq=1)
+        wandb.watch(policy, log = "all", log_freq = 1)
 
     policy.train()
     for episode in range(episodes):
@@ -55,12 +58,10 @@ def combo_reinforce(env, policy, lr = 1e-2, gamma = 0.999, episodes = 10, device
             action, log_prob = select_action(state, policy)
             states.append(state)
             log_probs.append(log_prob)
-
             state, reward, terminated, truncated, _ = env.step(action)
-            actions.append(torch.tensor(action, dtype = torch.long, device=device))
+            actions.append(torch.tensor(action, dtype = torch.long, device = device))
             rewards.append(reward)
             score += reward
-
             if terminated or truncated:
                 states.append(state)
                 break
@@ -71,14 +72,12 @@ def combo_reinforce(env, policy, lr = 1e-2, gamma = 0.999, episodes = 10, device
         optimizer.zero_grad()
         policy_loss.backward()
         optimizer.step()
-
         running_rewards.append(0.010 * discountedRewards[0] + 0.990 * running_rewards[-1])
 
         if wandb_log:
             wandb.log({"score": score,
                        "policy_loss": policy_loss,
-                       "running_reward": running_rewards[-1]}, step=episode)
-
+                       "running_reward": running_rewards[-1]}, step = episode)
         print(f'Episode {episode+1}, {len(rewards)}\tScore: {score:.2f}; Running reward: {running_rewards[-1]:.2f}')
     
     if wandb_log:
@@ -90,12 +89,11 @@ def combo_reinforce(env, policy, lr = 1e-2, gamma = 0.999, episodes = 10, device
 
 # The actual REINFORCE algorithm
 def combo_reinforce_with_baseline(env, policy, lr = 1e-2, gamma = 0.999, episodes = 10, device = 'cpu', wandb_log = False):
-
     optimizer = torch.optim.Adam(policy.parameters(), lr = lr)
     running_rewards = [0.0]
 
     if wandb_log:
-        wandb.watch(policy, log="all", log_freq=1)
+        wandb.watch(policy, log = "all", log_freq = 1)
 
     policy.train()
     for episode in range(episodes):
@@ -109,12 +107,10 @@ def combo_reinforce_with_baseline(env, policy, lr = 1e-2, gamma = 0.999, episode
             action, log_prob = select_action(state, policy)
             states.append(state)
             log_probs.append(log_prob)
-
             state, reward, terminated, truncated, _ = env.step(action)
-            actions.append(torch.tensor(action, dtype = torch.long, device=device))
+            actions.append(torch.tensor(action, dtype = torch.long, device = device))
             rewards.append(reward)
             score += reward
-
             if terminated or truncated:
                 states.append(state)
                 break
@@ -125,14 +121,12 @@ def combo_reinforce_with_baseline(env, policy, lr = 1e-2, gamma = 0.999, episode
         optimizer.zero_grad()
         policy_loss.backward()
         optimizer.step()
-
         running_rewards.append(0.005 * discountedRewards[0] + 0.995 * running_rewards[-1])
 
         if wandb_log:
             wandb.log({"score": score,
                        "policy_loss": policy_loss,
-                       "running_reward": running_rewards[-1]}, step=episode)
-
+                       "running_reward": running_rewards[-1]}, step = episode)
         print(f'Episode {episode+1}, {len(rewards)}\tScore: {score:.2f}; Running reward: {running_rewards[-1]:.2f}')
     
     if wandb_log:
