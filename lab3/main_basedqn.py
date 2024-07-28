6c56ac270c0f37996321defdd665cf01e7fa30cd
Giovanni Colombo <giovanni.colombo@edu.unifi.it>
2024-07-28 18:05:01 +0200

    Update and rename main_dqn.py to main_basedqn.py

--- lab3/main_basedqn.py
+++ lab3/main_basedqn.py
@@ -1,9 +1,10 @@
+# NOTE: my only contribution to this code was adding Weights & Biases compatibility for performance tracking.
+
 import gymnasium as gym
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-from torch.utils.tensorboard import SummaryWriter
 from gym_navigation.memory.replay_memory import ReplayMemory, Transition
 import numpy as np
 import math
@@ -11,20 +12,22 @@ import random
 
 import wandb
 
+
 TRAIN = True
+env_name = 'LunarLander-v2'     # 'gym_navigation:NavigationGoal-v0' or 'LunarLander-v2' or 'CartPole-v1' ...
 wandb_log = True
-EPS = 1.0  # the starting value of epsilon
-EPS_DECAY = 0.9999  # controls the rate of exponential decay of epsilon, higher means a slower decay
+
+EPS = 1.0                       # the starting value of epsilon
+EPS_DECAY = 0.9999              # controls the rate of exponential decay of epsilon, higher means a slower decay
 MIN_EPS = 0.0001
-GAMMA = 0.99  # Discount Factor
-BATCH_SIZE = 512  # is the number of transitions random sampled from the replay buffer
-LEARNING_RATE = 1e-3  # is the learning rate of the Adam optimizer, should decrease (1e-5)
+GAMMA = 0.99                    # Discount Factor
+BATCH_SIZE = 512                # is the number of transitions random sampled from the replay buffer
+LEARNING_RATE = 1e-3            # is the learning rate of the Adam optimizer, should decrease (1e-5)
+TEST_EPISODES = 500
 steps_done = 0
 hidden_sizes = [128, 128]
 
-# if gpu is to be used
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 
 class DQLN(nn.Module):
     def __init__(self, n_observations, n_actions):
@@ -32,16 +35,13 @@ class DQLN(nn.Module):
         self.layer1 = nn.Linear(n_observations, hidden_sizes[0])
         self.layer2 = nn.Linear(hidden_sizes[0], hidden_sizes[1])
         self.layer3 = nn.Linear(hidden_sizes[1], n_actions)
-
     def forward(self, x):
         x = F.relu(self.layer1(x))
         x = F.relu(self.layer2(x))
         return self.layer3(x)
 
-
 MAX_DISTANCE = 15.0
 MIN_DISTANCE = 0.2
-
 
 def normalize(state: np.ndarray) -> np.ndarray:
     nornmalized_state = np.ndarray(shape=state.shape, dtype=np.float64)
@@ -52,23 +52,16 @@ def normalize(state: np.ndarray) -> np.ndarray:
             nornmalized_state[i] = state[i] / math.pi
     return nornmalized_state
 
-
 # Training loop
 if TRAIN:
-
-    if wandb_log:
-        wandb.init(project="DLA_Lab3_DRL", monitor_gym=True, save_code=True)
-    
-    env = gym.make('LunarLander-v2', render_mode='human')
-    env.action_space.seed(42)  # 42
+    env = gym.make(env_name, render_mode = 'human')
+    env.action_space.seed(42)
 
     state_observation, info = env.reset(seed=42)
-
     # Get number of actions from gym action space
     n_actions = env.action_space.n
     # Get the number of state observations
     n_observations = len(state_observation)
-
     # Instantiate policy network.
     q_function = DQLN(n_observations, n_actions).to(device)
 
@@ -78,10 +71,8 @@ if TRAIN:
 
     # Use Adam to optimize.
     optimizer = optim.Adam(q_function.parameters(), lr=LEARNING_RATE, weight_decay=1e-5)
-
     # Using a larger replay buffer.
     replay_buffer = ReplayMemory(100000)
-
 
     # Epsilon-greedy action sampling.
     def select_action_epsilon(state):
@@ -93,7 +84,6 @@ if TRAIN:
         else:
             return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
 
-
     # Do one batch of gradient descent.
     def optimize_model():
         # Make sure we have enough samples in replay buffer.
@@ -102,7 +92,6 @@ if TRAIN:
 
         # Sample uniformly from replay buffer.
         transitions = replay_buffer.sample(BATCH_SIZE)
-
         # This converts batch-arrays of Transitions to Transition of batch-arrays.
         batch = Transition(*zip(*transitions))
 
@@ -126,14 +115,10 @@ if TRAIN:
         # Optimize the model
         optimizer.zero_grad()
         loss.backward()
-
-        writer.add_scalars('loss', {'policy_net': loss}, i_episode)
-
         # torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
         optimizer.step()
 
         return loss
-
 
     def run_validation(env, policy_net, num=10):
         running_rewards = [0.0] * num
@@ -150,17 +135,15 @@ if TRAIN:
                     break
         return running_rewards
 
-
     if torch.cuda.is_available():
         num_episodes = 1500
     else:
         num_episodes = 1500
 
-    # Writer will output to ./runs/ directory by default
-    writer = SummaryWriter("runs")
-
     if wandb_log:
-        wandb.watch(q_function, log="all", log_freq=1)
+        wandb.login()
+        wandb.init(project = "DLA_Lab3_DRL", monitor_gym = True, save_code = True, name = f"{env_name}_baseDQN_eps{EPS}_bs{BATCH_SIZE}_lr{LEARNING_RATE}_gamma{GAMMA}")
+        wandb.watch(q_function, log = "all", log_freq = 1)
 
     print("START Deep Q-Learning Navigation Goal")
 
@@ -172,23 +155,17 @@ if TRAIN:
         state_observation = torch.tensor(state_observation, dtype=torch.float32, device=device).unsqueeze(0)
         steps = 0
         score = 0
-
         # Run one episode.
         while True:
             action = select_action_epsilon(state_observation)
             observation, reward, terminated, truncated, _ = env.step(action.item())
             score += reward
-
             observation = normalize(observation)  # Normalize in [0,1]
-
             reward = torch.tensor([reward], device=device)
             done = terminated or truncated
-
             next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
-
             # Store the transition in memory
             replay_buffer.push(state_observation, action, next_state, reward, done)
-
             # Move to the next state
             state_observation = next_state
             steps += 1
@@ -198,7 +175,6 @@ if TRAIN:
 
         # Perform one step of the optimization (on the policy network)
         loss = optimize_model()
-
         # Epsilon decay
         EPS = max(MIN_EPS, EPS * EPS_DECAY)
 
@@ -210,21 +186,18 @@ if TRAIN:
         # Every 50 episodes, validate.
         if not i_episode % 50:
             rewards = run_validation(env, q_function)
-            writer.add_scalars('Reward', {'policy_net': np.mean(rewards)}, i_episode)
-            writer.add_scalars('Epsilon', {'policy_net': EPS}, i_episode)
 
         if wandb_log:
             wandb.log({"score": score,
                        "policy_loss": loss.item(),
-                       "running_reward": rewards[-1]}, step=i_episode)
+                       "running_reward": rewards[-1]}, step = i_episode)
 
-    PATH = './checkpoints/lunarlander.pt'
+    PATH = f'./checkpoints/{env_name}/baseDQN_{env_name}_eps{EPS}_bs{BATCH_SIZE}_lr{LEARNING_RATE}_gamma{GAMMA}.pth'
     torch.save(q_function.state_dict(), PATH)
 
     if wandb_log:
         wandb.finish()
-    
-    writer.close()
+
     env.close()
     print('COMPLETE')
 
@@ -232,7 +205,7 @@ else:
     # For accuracy check
     # env = gym.make('gym_navigation:NavigationGoal-v0', render_mode=None, track_id=1)
     # For visible check
-    env = gym.make('LunarLander-v2', render_mode='human')
+    env = gym.make(env_name, render_mode='human')
 
     env.action_space.seed(42)
     state_observation, info = env.reset(seed=42)
@@ -241,11 +214,10 @@ else:
     n_observations = len(state_observation)
 
     q_function = DQLN(n_observations, n_actions).to(device)
-    PATH = './checkpoints/lunarlander.pt'
+    PATH = f'./checkpoints/{env_name}/baseDQN_{env_name}_eps{EPS}_bs{BATCH_SIZE}_lr{LEARNING_RATE}_gamma{GAMMA}.pth'
     q_function.load_state_dict(torch.load(PATH))
     not_terminated = 0
     success = 0
-    TEST_EPISODES = 500
     for _ in range(TEST_EPISODES):
         steps = 0
         while True:
