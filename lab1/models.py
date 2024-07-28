e39ce8ee4e3868a74de68259e312477ded3a8b40
Giovanni Colombo <giovanni.colombo@edu.unifi.it>
2024-07-28 11:29:23 +0200

    Update models.py

--- lab1/models.py
+++ lab1/models.py
@@ -1,8 +1,12 @@
+# Deep Learning Applications 2023 course, held by Professor Andrew David Bagdanov - University of Florence, Italy
+# Created by Giovanni Colombo - Mat. 7092745
+# Dedicated Repository on GitHub at https://github.com/giovancombo/DLA_Labs/tree/main/lab2
+
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-
 
 # Multi-Layer Perceptron
 class MLP(nn.Module):
