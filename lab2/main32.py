7b7a14df21c66fd3264efe5316be5d3fe29e5549
Giovanni Colombo <giovanni.colombo@edu.unifi.it>
2024-07-28 16:23:29 +0200

    Update main32.py

--- lab2/main32.py
+++ lab2/main32.py
@@ -1,1 +1,5 @@
-# Work in progress
+# Deep Learning Applications 2023 course, held by Professor Andrew David Bagdanov - University of Florence, Italy
+# Created by Giovanni Colombo - Mat. 7092745
+# Dedicated Repository on GitHub at https://github.com/giovancombo/DLA_Labs/tree/main/lab2
+
+# Code for EXERCISE 3.2
